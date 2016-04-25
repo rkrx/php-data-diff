8c5b173994a0c09ffce58d8f39fac5d1a31ec609
Ron Kirschler <ron.kirschler@gmail.com>
2016-04-25 12:18:04 +0200

    - Added some missing methods to `DataDiff\DiffStorageStoreRowDataInterface`

--- src/DiffStorageStoreRowDataInterface.php
+++ src/DiffStorageStoreRowDataInterface.php
@@ -1,12 +1,20 @@
 <?php
 namespace DataDiff;
 
+use Exception;
+
 interface DiffStorageStoreRowDataInterface {
 	/**
 	 * @param array $options
 	 * @return array
 	 */
 	public function getData(array $options = []);
+
+	/**
+	 * @param array $options
+	 * @return array
+	 */
+	public function getForeignData(array $options = []);
 
 	/**
 	 * @param array $options
@@ -19,4 +27,18 @@ interface DiffStorageStoreRowDataInterface {
 	 * @return array
 	 */
 	public function getValueData(array $options = []);
+
+	/**
+	 * @param array $fields
+	 * @return array
+	 */
+	public function getDiff(array $fields = null);
+
+	/**
+	 * @param array $fields
+	 * @param mixed $format
+	 * @return array
+	 * @throws Exception
+	 */
+	public function getDiffFormatted(array $fields = null, $format = null);
 }
